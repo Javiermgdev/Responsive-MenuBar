c5c72645883930e232b1a5b9e646c20a17c40f94
Javiermgdev <javiermoyag81@gmail.com>
2020-11-18 15:43:46 +0000

    responsive menubar

---
+++ app.js
@@ -0,0 +1,9 @@
+addEventListener('DOMContentLoaded', () => {
+    const button_menu = document.querySelector('.button_menu')
+    if (button_menu) {
+        button_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu_items')
+            menu_items.classList.toggle('show')
+        })
+    }
+}) 
